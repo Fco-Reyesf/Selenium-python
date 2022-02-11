feab89151991ba79ff82a7c3b9bae74311a49c50
Fco-Reyesf <freyes.info@gmail.com>
2022-02-10 22:51:10 -0300

    comandos ejecutar 2

--- test/test_demo.py
+++ test/test_demo.py
@@ -10,16 +10,21 @@ Para ejecutar solo archivos en específico, se debe ingresar el nombre del archi
 
 py.test “nombreArchivo.py” -v -s
 
-para ejecutar pruebas en específico, 
-se debe agregar el parámetro “-k” y escribir un distintivo que tengan varias pruebas en común 
-(independiente del archivo del que proviene). 
-En este caso buscara en todos los archivos y ejecutara solo las pruebas que tengan en el nombre “resta”.
+para ejecutar pruebas en específico, se debe agregar el parámetro “-k” y escribir un distintivo que tengan varias pruebas en común (independiente del archivo del que proviene). En este caso buscara en todos los archivos y ejecutara solo las pruebas que tengan en el nombre “resta”.
+py.test  -k resta -v -s
 
-py.test -k resta -v -s
+También se pueden ejecutar las pruebas que se encuentran con marcas, en este caso en el archivo de “test_demo.py” una prueba cuenta con la marca “ejecutar”.
+py.test  -m ejecutar -v -s
+
+Para no ejecutar ciertas pruebas se puede usar una marca de “skip”, se encuentra comentado en el archivo de “test_demo.py”
+
 ------------------------------------------------------------------------
-
-
-
 '''
+
+import pytest
+
+
+@pytest.mark.ejecutar
+# @pytest.mark.skip
 def test_restaConDecimal():
     print("soy resta con decimal")
