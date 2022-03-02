241abe755708b161ba964210b5dc3058561a74b5
Fco-Reyesf <freyes.info@gmail.com>
2022-03-01 22:59:55 -0300

    parametros por consola

--- proyecto/test/conftest.py
+++ proyecto/test/conftest.py
@@ -2,10 +2,23 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.edge.service import Service
 
+# funcion para recibir parametros en la consola.
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="edge"
+    )
+
+
 @pytest.fixture(scope="class")
 def setup(request):
     global driver
-    driver = webdriver.Edge(service=Service("..\\..\\edge_driver_selenium\\edgedriver_win64\\msedgedriver.exe"))
+    browser_seleccionado = request.config.getoption("browser")
+    if browser_seleccionado == "chrome":
+        driver = webdriver.Chrome(service=Service("ruta para el driver"))
+    elif browser_seleccionado == "firefox":
+        driver = webdriver.Firefox(service=Service("ruta para el driver"))
+    elif browser_seleccionado == "edge":
+        driver = webdriver.Edge(service=Service("..\\..\\edge_driver_selenium\\edgedriver_win64\\msedgedriver.exe"))
     driver.get("https://rahulshettyacademy.com/angularpractice/")
     driver.maximize_window()
     request.cls.driver = driver
